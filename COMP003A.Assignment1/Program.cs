4b3b75b28eb49d0f9923b1ac8fb06dfa28bf614e
evanbern59 <evansantos1202@gmail.com>
2026-01-16 21:31:52 -0800

    added write lines 1-12

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -5,7 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things about Me!");
-           
+            Console.WriteLine("1. My Name is Evan");
+            Console.WriteLine("2. I am 20 Years Old");
+            Console.WriteLine("3. I am from Hanford");
+            Console.WriteLine("4. I like dogs");
+            Console.WriteLine("5. I Like Cats");
+            Console.WriteLine("6. I honestly dont know 100 things about me");
+            Console.WriteLine("7. My favorite color is Grey");
+            Console.WriteLine("8. I like to play games");
+            Console.WriteLine("9. Recently ive played BO7");
+            Console.WriteLine("10. I dont really enjoy school");
+            Console.WriteLine("11. I only do it to get paid");
+            Console.WriteLine("12. I enjoy music");
+   
+
         }
     }
 }
